0a97e975ff9b9140e9f7fcc1a49b2340ecf2760d
neel <neel2003dave@gmail.com>
2024-01-02 11:39:13 +0530

    first commit

--- src/student/db.js
+++ src/student/db.js
@@ -1,11 +1,10 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
 
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "students",
-  password: process.env.PASS,
+  password: "Neel",
   port: 5432,
 });
 
